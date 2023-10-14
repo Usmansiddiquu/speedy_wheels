2bfce89c49eec22053b91e51d74e16b2b60f043a
USMAN SIDDIQUI <121655186+Usmansiddiquu@users.noreply.github.com>
2023-10-14 15:30:15 +0500

    add files

---
+++ app.js
@@ -0,0 +1,225 @@
+var Cars = [
+
+    {   //audi a6
+        name: "audi",
+        Price: "2.65 - 5.3 crore",
+        Mileage: "9 - 16 KM/L",
+        Transmission: "Automatic",
+        Fuel_Type: "Petrol",
+        Fuel_Tank_Capacity: "63 - 87 L",
+        Engine: "2.0 liter Turbocharged Inline 4-cylinder",
+        Body_Type: "Sedan",
+        Dimensions: "4939 x 2110 x 1457 mm",
+        Ground_Clearance: "0 - 142 mm",
+        Displacement: "1987 - 2000 c",
+        Torque: "320 - 370 Nm",
+        Boot_Space: "434 - 530 L",
+        Kerb_Weight: "1635 - 2345 KG",
+        Seating_Capacity: "5 - Persons",
+        Top_Speed: "250 KM/H",
+        Tyre_Size: "245/45/19",
+        img: "Audi_A6"
+    },
+
+    {   // BMW X2 2023 Specifications
+        name: "bmw",
+        Price: "1.66 crore",
+        Mileage: "10 to 11 KM/litre",
+        Transmission: "Automatic",
+        Fuel_Type: "Petrol",
+        Fuel_Tank_Capacity: "52 L",
+        Engine: "1499 cc to 1998 cc",
+        Body_Type: "Crossover",
+        Dimensions: "(Length x Width x Height) 4360 x 1824 x 1526 mm",
+        Ground_Clearance: "182 mm",
+        Displacement: "1499 cc",
+        Horse_Power: "140 hp",
+        Torque: "220 Nm",
+        Boot_Space: "470 L",
+        Kerb_Weight: "1480 KG",
+        Seating_Capacity: "5 - Persons",
+        Top_Speed: "205 KM/H",
+        Tyre_Size: "225/55/R17",
+    },
+
+    {
+        name: "civic",
+        Price: "86.0 lacs - 1.02 crore",
+        Mileage: "11 - 14 KM/L",
+        Transmission: "Automatic",
+        Fuel_Type: "Petrol",
+        Fuel_Tank_Capacity: "47 L",
+        Engine: "1498cc DOHC dual VTC",
+        Body_Type: "Sedan",
+        Dimensions: "(Length x Width x Height) 4687 x 1802 x 1432 mm",
+        Ground_Clearance: "154 mm",
+        Displacement: "1500 cc",
+        Horse_Power: "127 - 176 hp",
+        Torque: "180 - 220 Nm",
+        Boot_Space: "409 L",
+        Kerb_Weight: "1396 KG",
+        Seating_Capacity: "5 - Persons",
+        Top_Speed: "210 - 260 KM/H",
+        Tyre_Size: "215/55/R16",
+    },
+
+    {
+        name: "proton",
+        Price: "66.0 - 71.3 lacs",
+        Mileage: "10 - 16 KM/L",
+        Transmission: "Automatic",
+        Fuel_Type: "Petrol",
+        Fuel_Tank_Capacity: "50 L",
+        Engine: "2.0-liter inline 4-cylinder MPI Gasoline DOHC",
+        Body_Type: "Sedan",
+        Dimensions: "4620 x 1800 x 1450 mm",
+        Ground_Clearance: "134 - 170 mm",
+        Displacement: "1600 - 2000 cc",
+        Torque: "155 - 195 Nm",
+        Boot_Space: "407 L",
+        Kerb_Weight: "1225 - 1255 KG",
+        Seating_Capacity: "5 - Persons",
+        Top_Speed: "180 - 240 KM/H",
+        Tyre_Size: "215/55/R16",
+    },
+
+    //     // Mercedes: {
+
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    // },
+
+    //     // Peugeot: {
+
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    // },
+
+    // {
+    //     // Proton: {
+
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    // },
+
+    //     // Porsche: {
+
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    // },
+
+    //     // suzuki: {
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    //     // },
+
+    //     // Toyota: {
+    //         Price: "",
+    //         Mileage: "11 - 14 KM/L",
+    //         Transmission: "Automatic",
+    //         Fuel_Type: "Petrol",
+    //         Fuel_Tank_Capacity: "",
+    //         Engine: "",
+    //         Body_Type: "Sedan",
+    //         Dimensions: "",
+    //         Ground_Clearance: "",
+    //         Displacement: "",
+    //         Torque: "",
+    //         Boot_Space: "",
+    //         Kerb_Weight: "",
+    //         Seating_Capacity: "5 - Persons",
+    //         Top_Speed: "",
+    //         Tyre_Size: "215/55/R16",
+    // },
+]
+
+
+// var filteredCars = [];
+// for (var i = 0; i < Cars.length; i++) {
+//     if (Cars[i].Top_Speed === '180 - 240 KM/H') {
+//         filteredCars = [...filteredCars, Cars[i]];
+//     }
+// }
+// console.log(filteredCars);
+
+function searchbar(){
+var searchvalue = document.getElementById("searchbar").value.toLowerCase()
+var filteredCars = [];
+for (var i = 0; i < Cars.length; i++) {
+    if (Cars[i].name === searchvalue) {
+        filteredCars = [...filteredCars, Cars[i]];
+    }
+}
+console.log(filteredCars);
+    }
+searchbar()
